a148286df73afc42b19ed1c7fec775780ea55707
kbpsystem777 <koleen.bp@outlook.com>
2018-11-20 02:08:30 +0800

    refactoring Contact.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.jpg';
-
-class Facebook extends Component {
-  render() {
-    return(
-      <div className="btn">
-        <a href="https://facebook.com/hanssosweet/" target="_blank" rel="noopener noreferrer">
-          <button className="fa fa-facebook"> Facebook</button>
-        </a>
-      </div>
-    )
-  }
-}
-
-
-class Instagram extends Component {
-  render() {
-    return(
-      <div className="btn">
-        <a href="https://www.instagram.com/hanssosweet_/" target="_blank" rel="noopener noreferrer">
-          <button className="fa fa-instagram"> Instagram</button>
-        </a>
-      </div>
-    )
-  }
-}
-
-
-
-class Contact extends Component {
-  render() {
-    return(
-      <div className="container">
-        <div>
-          <p className="Contact">
-            Contact Us:
-          </p>
-          <h5 className="Contact">GLOBE: 0905 4847198</h5>
-          <h5 className="Contact">SMART: 0999 7542792</h5>
-        </div>
-        <div>
-          <Facebook />
-          <Instagram />
-        </div>
-      </div>
-    )
-  }
-}
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
@@ -56,12 +10,12 @@ class App extends Component {
         <div className="jumbotron">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h3>
+            <p>
               Your Source of Locally-Priced Imported Sweets
             <div className="row">
                 <Contact />
               </div>
-            </h3>
+            </p>
           </header>
         </div>
       </div>
