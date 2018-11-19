f7c5d6a7aac7ded1536a4507f3311f9c840e985c
kbpsystem777 <koleen.bp@outlook.com>
2018-11-19 19:51:43 +0800

    adding bootstrap jumbotron

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
-const logo = 'https://scontent.fmnl7-1.fna.fbcdn.net/v/t1.0-9/44129394_244408512896552_6034732683592990720_n.jpg?_nc_cat=101&_nc_ht=scontent.fmnl7-1.fna&oh=0af2bea6272943643b0a1de3da71f881&oe=5C7834A3';
-
-
+import logo from './logo.jpg';
 
 class Facebook extends Component {
   render() {
     return(
       <div className="btn">
-        <a href="https://facebook.com/hanssosweet/" target="_blank">
-          <button className="fa fa-facebook btn-lg"> Facebook</button>
+        <a href="https://facebook.com/hanssosweet/" target="_blank" rel="noopener noreferrer">
+          <button className="fa fa-facebook"> Facebook</button>
         </a>
       </div>
     )
@@ -22,8 +19,8 @@ class Instagram extends Component {
   render() {
     return(
       <div className="btn">
-        <a href="https://www.instagram.com/hanssosweet_/" target="_blank">
-          <button className="fa fa-instagram btn-lg"> Instagram</button>
+        <a href="https://www.instagram.com/hanssosweet_/" target="_blank" rel="noopener noreferrer">
+          <button className="fa fa-instagram"> Instagram</button>
         </a>
       </div>
     )
@@ -56,15 +53,17 @@ class App extends Component {
   render() {
     return (
       <div className="App container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h3>
-            Your Source of Locally-Priced Imported Sweets
+        <div className="jumbotron">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h3>
+              Your Source of Locally-Priced Imported Sweets
             <div className="row">
-              <Contact/>
-            </div>
-          </h3>
-        </header>
+                <Contact />
+              </div>
+            </h3>
+          </header>
+        </div>
       </div>
     );
   }
