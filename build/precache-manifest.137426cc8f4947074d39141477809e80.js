92ae2decf1f900264e16b9aca8acd2f6716acde1
kbpsystem777 <koleen.bp@outlook.com>
2019-01-14 01:22:16 +0800

    Adding build dist

---
+++ build/precache-manifest.137426cc8f4947074d39141477809e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "223ba821cdb0af5b5e0da0051c49287a",
+    "url": "/static/media/logo.223ba821.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "55493e5e5597fa0a9af8",
+    "url": "/static/js/main.55493e5e.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "55493e5e5597fa0a9af8",
+    "url": "/static/css/main.dc92e117.chunk.css"
+  },
+  {
+    "revision": "6ccc11ca944ee5ba97fb4218dedfe397",
+    "url": "/index.html"
+  }
+];
